cb4bc96a328b6bb097df92b4a39514af7a76d1de
Jose Pirangaba <pirangabaneto@gmail.com>
2019-10-24 14:57:34 -0200

    corrigindo as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'logins/index'
+
+  get 'clientes/perfil'
+
+  get 'funcionarios/perfil'
+
+  get 'logins/sair'
+
+  get 'funcionarios/perfil'
+
+  get 'padrao/contato'
+
+  get 'funcionarios/perfil'
+
+  resources :contums
+  resources :funcionarios
+  resources :clientes
+  resources :logins
+
+  root 'logins#index'
 end
