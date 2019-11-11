ccfcbc1602d27769b090f1d65027c67c1ae7ca1e
David <davidmanuelpe@gmail.com>
2019-11-11 18:07:10 -0300

    tests model menu feitos

--- test/models/menu_test.rb
+++ test/models/menu_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class MenuTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should save menu" do
+    menu = Menu.new title: "testing", description: "testing is awesome"
+    assert menu.save
+  end
+
+  test "should not save menu without title" do
+    menu = Menu.new title: "", description: "testing is awesome"
+    assert_not menu.save
+  end
+
+  test "should not save menu without description" do
+    menu = Menu.new title: "testing", description: ""
+    assert_not menu.save
+  end
+
+  test "should destroy menu" do
+    menu = Menu.new title: "testing", description: "testing is awesome"
+    menu.save
+    assert menu.destroy
+  end
+
+  test "should save item inside menu" do
+    menu = Menu.new title: "testing", description: "testing is awesome"
+    menu.save
+    item = 'item teste'
+    menu.item << item
+    assert menu.save
+  end
+
+  test "should delete item inside menu" do
+    menu = Menu.new title: "testing", description: "testing is awesome"
+    menu.save
+    item = 'item teste'
+    menu.item << item
+    menu.save
+    menu.item.delete(item)
+    assert menu.save
+  end
+
 end
